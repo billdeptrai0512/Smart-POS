7feeb4801a6a36633182649cd45deda40863eb9e
billdeptrai0512 <billnguyen05121998@gmail.com>
2026-04-02 11:25:03 +0700

    pwa update

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Analytics } from '@vercel/analytics/react'
 import App from './App.jsx'
+import PWAUpdatePrompt from './components/PWAUpdatePrompt.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <PWAUpdatePrompt />
     <Analytics />
   </StrictMode>,
 )
